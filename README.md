f7463ea3f349f20fada11811134a13bd5b3a2187
Marc Corbalan Font <mcorbalanfont@gmail.com>
2026-05-21 15:53:19 +0200

    añadido modelos necesarios basicos, pequeños arreglos

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+##commit history 
+-------------------------------------------------------------
 First commit:
 Asegurar la base tecnologica del proyecto, arquitectura monolitica modular. Monorepo
 React Vite + Django DRF
@@ -39,3 +41,19 @@ he optado por no utilizar esta funcion experimental por una version mas estable 
 Empezaremos por el backend:
 primero hay que establecer una base para todo lo que vendremos a hacer, instalar el corsheader para que react no tenga problemas al hacer fetch con nosotros, seguidamente, tenemos que acabar de configurar todas las apps y aplicar nombres y buenas practicas en los settings de django.
 crear y asegurar que django rest framework funciona con el endpoint health, asegurandome que el flow del framework es correcto y para futuras consultas, y un endpoint de aservicio para controlar el versionado de la api en la que se esta trabajando.
+----
+
+Estructurar la base de datos:
+vamos a establecer los modelos para nuestro backend. 
+vamos a separar responsabilidades por funcionalidades, podemos entender lo siguiente:
+un dispensador contiene bebida, y el dispensador tiene unos usos (metricas)
+por lo tanto obtenemos 3 modelos:
+-Dispenser
+-DispenserUsage
+-Drink
+
+en produccion y con un volumen elevado de cervezas, o dispensadores, deberiamos colocar uuid como como pk, pero al tener un volumen un autoincremental es la mejor opcion.
+
+para los dispensadores creo una clase abstracta para evitar duplicaciones de campos hardcodeados, created_at  se repite, podemos obviar el updated_at, aun asi si quisieramos, con este cambio podemos escalar facilmente simplemente modificando esta clase que hemos creado.
+
+corroboro tambien que las migraciones con makemigrations i migrate funcionan correctamente y no rompen nada.
--- backend/django/apps/dispensers/models.py
+++ backend/django/apps/dispensers/models.py
@@ -1,3 +1,68 @@
 from django.db import models
-
+from apps.drinks.models import Drink
 # Create your models here.
+from django.utils import timezone
+from decimal import Decimal
+
+
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Dispenser(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    drink = models.ForeignKey(
+        Drink, 
+        on_delete=models.SET_NULL, 
+        related_name='dispensers',
+        null=True, 
+        blank=True
+    )
+    
+    flow_volume = models.FloatField(help_text="Flow volume in liters per second")
+    is_open = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+
+
+class DispenserUsage(TimeStampedModel):
+    dispenser = models.ForeignKey(Dispenser, on_delete=models.CASCADE, related_name='usages')
+    
+    opened_at = models.DateTimeField()
+    closed_at = models.DateTimeField(null=True, blank=True)
+    duration_seconds = models.FloatField(null=True, blank=True)  # Duration in seconds
+    liters_served = models.FloatField(null=True, blank=True)  # Liters served
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0, null=True, blank=True)
+    
+    def __str__(self):
+        return f"Usage of {self.dispenser.name} from {self.opened_at} to {self.closed_at or 'now'}"
+    
+    
+    @property
+    def is_active(self):
+        return self.closed_at is None
+    
+    def close_usage(self):
+
+        self.closed_at = timezone.now()
+
+        duration = (
+            self.closed_at - self.opened_at
+        ).total_seconds()
+
+        self.duration_seconds = duration
+
+        self.liters_served = (
+            duration * self.dispenser.flow_volume
+        )
+
+        self.total_price = Decimal(
+            self.liters_served *
+            float(self.dispenser.drink.price_per_liter)
+        )
+
+        self.save()
